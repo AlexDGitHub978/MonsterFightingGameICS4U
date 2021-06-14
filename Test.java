4c8ce139612b2e9ec10d1f5316fa80f43acbf5d0
Alexander Duong <cduon1@ocdsb.ca>
2021-06-14 16:28:38 -0400

    This is testing how to work with a shared project.

--- Test.java
+++ Test.java
@@ -1,19 +1,19 @@
 
 /**
- * Write a description of class TesterClass here.
+ * Write a description of class Test here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-public class TesterClass
+public class Test
 {
     // instance variables - replace the example below with your own
     private int x;
 
     /**
-     * Constructor for objects of class TesterClass
+     * Constructor for objects of class Test
      */
-    public TesterClass()
+    public Test()
     {
         // initialise instance variables
         x = 0;
