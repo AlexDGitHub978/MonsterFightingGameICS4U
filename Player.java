a52bf1654eda1f0f53f03fc2a8e8055431216457
Atchya-Nandan15 <47332103+AtchyaNandan15@users.noreply.github.com>
2021-06-15 13:19:24 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,14 +1,31 @@
 /**
- * Description: This is a class for the monster object
+ * Player is where the players name and health is tracked and kept. 
+ * It has a method that gets the health of the player.
+ * It has a method that buffs the players health with a random number.
+ * The strength is going to increase with random values with potions. 
  *
- * @author
- * @version 2021-06-03
- *
+ * @author (Atchya Nandan And Vincent Zhu) 
+ * @version Start: June 15th, 2021 
  */
 
-//declaring the monster class
-public class Player {
+public class Player
+{
+  //declare instane variables
+  String strName;
+  int intHealth;
+  int intATK; 
+  
+  //create get method for name and health
+  
+  
+  //create method that asks the user for there name
+  
+  
+  //create a method that gets the health of the player and reutns the health
+  
+  
 
-
-
+  //create a method to buff the player's healthu by a random value 
+  
+  
 }
