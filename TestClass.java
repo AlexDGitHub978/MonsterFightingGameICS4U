72b0bc9dc979e85c794e59e2836c4aafa5a4a748
Atchya-Nandan15 <47332103+AtchyaNandan15@users.noreply.github.com>
2021-06-16 09:26:41 -0400

    Update TestClass.java

--- TestClass.java
+++ TestClass.java
@@ -1,1 +1,25 @@
 
+/**
+ * In the test class we ask the user if they want to play and repeat the game. 
+ *
+ * @author (Atchya Nandan) 
+ * @version Start: June 16th 2021 
+ */
+//import java Scanner
+import java.util.Scanner; 
+public class TestClass
+{
+   public static void main(String[] args)
+   {
+       //delclare variables 
+       String strStart; 
+       
+       //ask the user if they want to play the monster fighting game 
+       System.out.println("Would you like to play the Monster fighting game"); 
+       strStart = new Scanner(System.in).nextLine(); 
+       
+       
+       
+       
+   }
+}
