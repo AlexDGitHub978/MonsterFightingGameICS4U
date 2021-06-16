a9cf6ca3ee24db799ddd1f49dfb25ab391cfd38a
KarimB03 <85885602+KarimB03@users.noreply.github.com>
2021-06-16 10:13:40 -0400

    Update TestClass.java

--- TestClass.java
+++ TestClass.java
@@ -13,13 +13,28 @@ public class TestClass
    {
        //delclare variables 
        String strStart; 
+       String playAgain = "";
+       boolean bolPlayAgain = true;
        
        //ask the user if they want to play the monster fighting game 
        System.out.println("Would you like to play the Monster fighting game"); 
        strStart = new Scanner(System.in).nextLine(); 
        
-       
-       
+       do 
+    {
+         
+        System.out.println("Do you want to play again [Y/N]?");
+        playAgain = new Scanner(System.in).nextLine();
+        if (playAgain.equalsIgnoreCase("Y"))
+            bolPlayAgain=false;
+        else if (playAgain.equalsIgnoreCase("N"))
+             bolPlayAgain=false;
+        else{
+            System.out.println("Please enter Y or N");
+            bolPlayAgain = true;
+        }
+    }while(bolPlayAgain);
+            
        
    }
 }
