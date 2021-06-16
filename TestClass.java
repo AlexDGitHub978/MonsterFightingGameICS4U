be224467016dbbea1f61c28781895c1c24a398fc
Atchya-Nandan15 <47332103+AtchyaNandan15@users.noreply.github.com>
2021-06-16 10:29:32 -0400

    Update TestClass.java

--- TestClass.java
+++ TestClass.java
@@ -1,8 +1,7 @@
-
 /**
  * In the test class we ask the user if they want to play and repeat the game. 
  *
- * @author
+ * @author (Atchya Nandan) 
  * @version Start: June 16th 2021 
  */
 //import java Scanner
@@ -12,30 +11,64 @@ public class TestClass
    public static void main(String[] args)
    {
        //delclare variables 
-       String strStart; 
-       String playAgain = "";
-       boolean bolPlayAgain = true;
+       String strStart;
+       String strplayAgain; 
+       boolean bolTryAgain = false;
+       boolean bolAgain = false;
+       boolean bolPlayAgain = false; 
+    
+       do
+       {
+          //ask the user if they want to play the monster fighting game 
+          System.out.println("Would you like to play the Monster fighting game\nEnter Start or enter Exit");
        
-       //ask the user if they want to play the monster fighting game 
-       System.out.println("Would you like to play the Monster fighting game"); 
-       strStart = new Scanner(System.in).nextLine(); 
-      System.out.println("Testing");
-       
-       do 
-    {
-         
-        System.out.println("Do you want to play again [Y/N]?");
-        playAgain = new Scanner(System.in).nextLine();
-        if (playAgain.equalsIgnoreCase("Y"))
-            bolPlayAgain=false;
-        else if (playAgain.equalsIgnoreCase("N"))
-             bolPlayAgain=false;
-        else{
-            System.out.println("Please enter Y or N");
-            bolPlayAgain = true;
-        }
-    }while(bolPlayAgain);
+          //use the scanner to start
+          strStart = new Scanner(System.in).nextLine();
+          
+          if(strStart.equalsIgnoreCase("start"))
+          {
+              
+          do
+          {
+             //add a new game
+             System.out.println("NEW GAME");
+             
+             //create a new game 
+             new game(); 
             
+            do 
+            {  
+            System.out.println("Do you want to play again [Y/N]?");
+            strplayAgain = new Scanner(System.in).nextLine();
+            if (strplayAgain.equalsIgnoreCase("Y"))
+            {
+                bolPlayAgain=false;
+                bolAgain = true; 
+            }
+            else if (strplayAgain.equalsIgnoreCase("N"))
+            {
+             bolPlayAgain=false;
+             bolAgain = false;
+             bolTryAgain = false; 
+            }
+            else
+            {
+              System.out.println("Please enter Y or N, Try Again");
+              bolPlayAgain = true;
+            }
+        }while(bolPlayAgain);
+        }while(bolAgain); 
+          }
+          else if(strStart.equalsIgnoreCase("Exit"))
+          {
+              bolTryAgain = false; 
+          }
+          else 
+          {
+            bolTryAgain = true; 
+          }
+       }while(bolTryAgain); 
+       
        
    }
 }
