3673c44f3de09af45b2032d797b9503cee3f61cf
MIRACLESHEEP <jpa.khalife@gmail.com>
2021-06-17 14:04:33 -0400

    created file output and game method

--- Game.java
+++ Game.java
@@ -12,8 +12,15 @@
 import java.util.Scanner;
 
 //declaring the monster class
-public class Game 
+public class Game
 {
+
+
+//static variable that counts the number of game instances created
+private static int intGamenum = 0;
+
+
+
 
   /**
    * Description: This method asks the user once whether they would
@@ -32,13 +39,13 @@ public class Game
       //ask the user if they would like to see
       //the tutorial
       System.out.println("Would you like a tutorial?");
-      
+
       //add a do while loop to check for valid user input
       do
       {
         //populate strUserInput
         this.strUserInput = new Scanner(System.in).nextLine();
-        
+
         //check if the user input is valid
         if(this.strUserInput.equalsIgnoreCase("yes"))
         {
@@ -55,7 +62,7 @@ public class Game
           System.out.println("You can play more than one game.");
           System.out.println("The results will be written to a file after each game.");
           System.out.println("Good luck!");
-          
+
           //reset bolUserInput to false to break out of the loop
           this.bolUserInput = false;
         }
@@ -68,18 +75,190 @@ public class Game
         {
           //output error message
           System.out.println("Error. Please enter yes or no.");
-          
+
           //set bolUserInput to true to loop again
           this.bolUserInput = true;
         }
       }while(this.bolUserInput);
-      
+
       //set bolTutorial to false to not ask the user
       //more than once
       bolTutorial = false;
     }
   }
-  
+
+
+  /**
+   * Description: This method is meant to run the game itself
+   *
+   * @author John Khalife
+   * @version 2021-06-16
+   *
+   */
+  public void runGame() {
+
+  //boolean that checks if the player is dead
+  boolean boolPlayerDead = false;
+
+  //boolean for thw hile loop that controls individual battles
+  boolean boolMonsterBattle = true;
+
+  //integer that checks for how many monsters were defeated if the player loses
+  int intMonstersDefeated = 0;
+
+  //integer that counts the number of rounds boolPlayerDead
+  int intRoundsPlayed = 0;
+
+  //For loop that iterates through the array of monsters
+  for(int i = 0 ; i < monsters.length ; i++) {
+
+      //this is the while loop that will control individual monster battles
+      boolMonsterBattle = true;
+      while(boolMonsterBattle) {
+
+      //calling the battle function and passing in the current monster that the player is fighting
+        battle(monsters[i]);
+        System.out.println( player.getstrName + ", your health is " + player.getintHealth());
+        System.out.println( player.getstrName + ", your attack is " + player.getIntATK());
+        intRoundsPlayed++;
+
+      //checking when the monster's or player's health is equal to zero
+      if (player.getintHealth <= 0) {
+        //informing the user of thei passing and eding the game
+        boolMonsterBattle = false;
+        boolPlayerDead = true;
+        System.out.println( player.getstrName + ", you have been defeated!");
+
+      } else if (monsters[i].getintHealth <= 0) {
+        //moving on to the next monster and informing the user
+        boolMonsterBattle = false;
+        System.out.println( "The " + monster.getName + " has been defeated!");
+      }
+
+
+      }
+
+      //adding one to the number of monsters defeated
+      intMonstersDefeated ++;
+
+     //checking if the player has been defeated
+     if(boolPlayerDead) {
+        System.out.println( "Better luck next time!");
+        i = monsters.length;
+     } else {
+        //deciding if the player deserves a health or shield buff
+
+     }
+
+  }
+
+  printResults(boolPlayerDead, intMonstersDefeated, intRoundsPlayed);
+
+  }
+
+    /**
+     * Description: This method outputs the result of the match into a file
+     *
+     * @author John Khalife
+     * @version 2021-06-16
+     *
+     */
+     public void printResults(boolean boolPlayerDead, int intMonstersDefeated, int intRoundsPlayed) {
+
+
+     //outputting a small amount of info about the game to the user
+     System.out.println("Well played, " + player.getstrName() + "!");
+
+
+     System.out.println("This was game #" + intGamenum);
+     if (boolPlayerDead) {
+       System.out.println("You fought " + intMonstersDefeated + " monsters out of " + monsters.length + "!");
+     } else {
+       System.out.println("You defeated " + intMonstersDefeated + " monsters!");
+     }
+
+     System.out.println("You played " + intRoundsPlayed + " rounds!");
+     System.out.println("Your attack value at the end of the game was " + player.getIntATK);
+     System.out.println("Your health value at the end of the game was " + player.getIntHealth();
+
+     System.out.println("Check the text file corresponding to this game to see more statistics relating to your game!");
+
+
+    //creating a file for the information to be outputted to
+    //try catch for creating file
+            try {
+
+                //Creating file object
+                File myObj = new File("MonsterFightingGame_#" + intGameNum + ".txt");
+
+                //Checking if file has been created
+                if (myObj.createNewFile()) {
+                    System.out.println("Created file " + myObj.getName());
+                }
+
+                //If error is caught - trace the source
+            } catch (IOException e) {
+                System.out.println("An error occured in the creation of this file.");
+                e.printStackTrace();
+            }
+
+            //try catch for writing to file
+            try {
+
+                //creating filewriter object
+                FileWriter myWriter = new FileWriter("MonsterFightingGame_#" + intGameNum + ".txt");
+
+                //displaying winner
+                if (boolPlayerDead) {
+                    myWriter.write("Winner: " + monsters[intMonstersDefeated - 1].getName() + "\n\n");
+                } else {
+                    myWriter.write("Winner: " + player.getstrName() +"\n\n");
+                }
+
+                myWriter.write("Game #" + intGamenum + "\n");
+
+                     if (boolPlayerDead) {
+                        myWriter.write("Fought " + intMonstersDefeated + " monsters out of " + monsters.length + "\n");
+
+                     } else {
+                       myWriter.write("Defeated " + intMonstersDefeated + " monsters \n");
+                     }
+
+
+
+
+                myWriter.write(intRoundsPlayed + " rounds played \n");
+                myWriter.write("Player attack value at end of game:" +  player.getIntATK() + "\n");
+                myWriter.write("Player health value at end of game:" +  getIntHealth() + "\n\n");
+
+
+
+
+                //displaying all monsters
+                myWriter.write("All monsters:\n\n");
+
+                //loop to print all monsters stats
+                for (int i = 0; i < monsters.length ; i++) {
+
+                    myWriter.write("Monster #" + (i+1) + "\n");
+                    myWriter.write("Name: " + monsters[i].getName() + "\n");
+                    myWriter.write("Attack: " + monsters[i].getAttack() + "\n");
+                }
+
+                //close the writer
+                myWriter.close();
+                //error catching
+            } catch (IOException e) {
+                System.out.println("An error occured in writing the file.");
+                e.printStackTrace();
+            }
+
+
+
+
+     }
+
+
   /**
    * Description: This method asks the user how many monsters they would
    * like to fight. It then creates that many monster instances in an array.
@@ -92,7 +271,7 @@ public class Game
   {
     //ask the user how many monsters they would like to fight
     System.out.println("How many monsters would you like to fight?");
-    
+
     //add a do while loop to loop the try and catch block
     do
     {
@@ -101,7 +280,7 @@ public class Game
       {
         //populate bytUserInput and parse to byte
         this.bytUserInput = new Scanner(System.in).nextByte();
-        
+
         //check if the input is at least 1
         if(this.bytUserInput > 0)
         {
@@ -112,7 +291,7 @@ public class Game
         {
           //output error message
           System.out.println("Error. Please enter a number more than 0.");
-          
+
           //set bolUserInput to true to loop again
           this.bolUserInput = true;
         }
@@ -121,41 +300,41 @@ public class Game
       {
         //output error message
         System.out.println("Error. Please enter a non-decimal number.");
-        
+
         //set bolUserInput to true to loop again
         this.bolUserInput = true;
       }
     }while(this.bolUserInput);
-    
+
     //initialize the monsters array
     monsters = new Monster[this.bytUserInput];
-    
+
     //use a for loop to create the monster instances and put them in the array
     for(int i = 0; i < monsters.length; i ++)
     {
-      //create the monster instances by calling the default constructor 
+      //create the monster instances by calling the default constructor
       monsters[i] = new Monster();
     }
   }
-  
+
   //Battle system
   public static void battle(Player player, Monster monster)
   {
       //variables to store player/moster move
       String playerMove;
       int monsterMove;
-        
+
       //randomize monster move
       monsterMove=(int)(Math.random()*2);
       /* monsterMove 0 = Quick Slice
        * monsterMove 1 = Shield Bash
        * monsterMove 3 = Mighty Slam
        */
-        
+
       //prompt the user to enter their move
       System.out.println("Your next move will be:\n Quick Slice  /  Shield Bash  /  Mighty Slam");
       playerMove= new Scanner(System.in).nextLine();
-        
+
       //possible reactions when player used quick slice
       if (playerMove.equalsIgnoreCase("Quick Slice"))
       {
@@ -166,7 +345,7 @@ public class Game
               player.intHealth -= monster.intDamage;
               //monster damaged
               monster.intHealth -= player.intATK;
-                
+
               //output damage message
               System.out.println("You used Quick Slice! Dealt " + player.intATK + "damage to the monster!");
               System.out.println("The Monster uses Quick Slice! Dealt " + monster.intDamage + "damage to you!");
@@ -176,19 +355,19 @@ public class Game
           {
               //player damaged
               player.intHealth -= monster.intDamage;
-                
+
               //output damage message
               System.out.println("The Monster uses Shield Bash! Blocking you Quick Slice and Dealt " + monster.intDamage + "damage to you!");
-          }  
+          }
           //monster used Mighty Slam
           else if(monsterMove == 2)
           {
               //monster damaged
               monster.intHealth -= player.intATK;
-                
+
               //output damage message
               System.out.println("You used Quick Slice! Dodging the monster's Mighty Slam and Dealt " + player.intATK + "damage to the monster!");
-          }          
+          }
       }
       //possiable reactions when player used Shield Bash
       else if (playerMove.equalsIgnoreCase("Shield Bash"))
@@ -198,7 +377,7 @@ public class Game
           {
               //monster damaged
               monster.intHealth -= player.intATK;
-                
+
               //output damage message
               System.out.println("You used Shield Bash! Blocking the monster's Quick Slice and Dealt " + player.intATK + "damage to the monster!");
           }
@@ -207,18 +386,18 @@ public class Game
           {
               //output damage message
               System.out.println("You used Shield Bash! Coliding with the monster's Shield Bash, no damage taken!");
-          }  
+          }
           //monster used Mighty Slam
           else if(monsterMove == 2)
           {
               //player damaged
               player.intHealth -= monster.intDamage;
-                
+
               //output damage message
               System.out.println("The monster used Mighty Slam penetrating your shield bash and Dealt " + monster.intDamgage + "damage to the monster!");
-          }          
+          }
       }
-        
+
       //possible reactions when player used Mighty Slam
       else if (playerMove.equalsIgnoreCase("Mighty Slam"))
       {
@@ -227,7 +406,7 @@ public class Game
           {
               //player damaged
               player.intHealth -= monster.intDamage;
-                
+
               //output damage message
               System.out.println("The monster used Quick Slice to dodge your Mighty Slam! Dealing" + player.intATK + "damage to you!");
           }
@@ -236,10 +415,10 @@ public class Game
           {
               //monster damaged
               monster.intHealth -= player.intATK;
-                
+
               //output damage message
               System.out.println("You used Mighty Slam to penetrate through the monster's Shield Bash! Dealing" + player.intATK + "damage to the monster!");
-          }  
+          }
           //monster used Mighty Slam
           else if(monsterMove == 2)
           {
@@ -247,11 +426,11 @@ public class Game
               player.intHealth -= monster.intDamgage;
               //monster damaged
               monster.intHealth -= player.intATK;
-                
+
               //output damage message
               System.out.println("You used Mighty Slam! Dealing" + player.intATK + "damage to the monster!");
               System.out.println("The monster used Mighty Slam! Dealing" + monster.intDamgage + "damage to you!");
-          }          
+          }
       }
       //restart method if player entered wrong input
       else
--- Player.java
+++ Player.java
@@ -1,139 +1,140 @@
 /**
- * Player is where the players name and health is tracked and kept. 
+ * Player is where the players name and health is tracked and kept.
  * It has a method that gets the health of the player.
  * It has a method that buffs the players health with a random number.
- * The strength is going to increase with random values with potions. 
+ * The strength is going to increase with random values with potions.
  *
- * @author (Atchya Nandan And Vincent Zhu) 
- * @version Start: June 15th, 2021 
+ * @author (Atchya Nandan And Vincent Zhu)
+ * @version Start: June 15th, 2021
  */
 
 //import java Scanner
+
 import java.util.Scanner;
 
 //import java math
-import java.lang.Math; 
-public class Player
-{
-  /*
-   * Atchya Nandan, I am creating instance variable right here. 
-   */
-  //declare instance variables
-  private String strName;
-  private int intHealth;
-  private int intATK;
-  
-  //declare variables for random numbers 
-  int randHPBuff;
-  int randATKBuff;
-  
-  /*
-   * Atchya Nandan, I am creating a method for players name 
-  */
-  
-  //get method for players name 
-  public String getstrName()
-  {
-      return this.strName(); 
-  }
-  
-   /*
-   * Atchya Nandan, I am creating a set method for the name 
-  */
-  
-  //set method for players name
-  public void setstrName(String s)
-  {
-      this.strName = s; 
-  }
-  
-  /*
-   * Atchya Nandan, I am creating a method that uses health and sets the base value to 100 
-  */
-  
-  //create a method that gets the health of the player and reutns the health
-  public void health()
-  {
-      //sets the base value of the health to 100
-      this.intHealth = 100; 
-  }
-  
-  /*
-  * Atchya Nandan, I am creating a method that uses ATK and sets the base value to 35
-  */
-  
-   //create method that sets ATK to 35
-  public void ATK()
-  {
-      //sets the base value of ATK to 35 
-      this.intATK = 35; 
-  }
-  
-  /*
-  * Atchya Nandan, I am creating a get method for the health
-  */
-  
-  //get method for players health 
-  public int getintHealth()
-  {
-      return this.intHealth(); 
-  }
-  
+import java.lang.Math;
+
+public class Player {
+    //declare variables for random numbers
+    int randHPBuff;
+    int randATKBuff;
     /*
-   * Atchya Nandan, I am creating a name method that asks the
-   * user for their name and then sets the name in the
-   * instance variable 
-  */
+     * Atchya Nandan, I am creating instance variable right here.
+     */
+    //declare instance variables
+    private String strName;
+    private int intHealth;
+    private int intATK;
 
-  //create method that asks the user for there name
-  public void name()
-  {
-      //declare variable for the players name 
-      String strPlayerName;
-      
-      //declare a variable of type Player
-      Player name = new Player(); 
-      
-      //ask the user for enter name
-      System.out.println("****************"); 
-      System.out.println("Enter your name");
-      System.out.println("****************"); 
-      
-      //use the scanner to get name 
-      strPlayerName = new Scanner(System.in).nextLine(); 
-      
-      //set the users name with set method 
-      name.setstrName(strPlayerName); 
-      
-  } 
-  
-  //Vincent *
-  //create a method to buff the player's health by a random value
-  public static void PlayerHPBuff(int health, int randHPBuff)
-  {
-      //create a random number for buff
-      randHPBuff = (int) ((Math.random()*6) +1) *10;
-      
-      //add random number to health value 
-      health += randHPBuff;
-    
-      //print message notifiying player incrase of Health stats
-      System.out.println("you've got a health potion! your health has increased "+ randHPBuff);
-  }
+    /*
+     * Atchya Nandan, I am creating a method for players name
+     */
 
-  //create a method that increases the players strength by a random value with potions 
-  public static void PlayerATKBuff(int ATK, int randATKBuff)
-  {
-      //create a random number for the strength 
-      randATKBuff = (int) ((Math.random()*6)+1) *2;
-      
-      //add random number to the health 
-      ATK += randATKBuff;
-    
-      //print message notifiying player incrase of ATK stats
-      System.out.println("you've recieved a strength potion! your health has increased "+ randATKBuff);
-      
-  }
+    //Vincent *
+    //create a method to buff the player's health by a random value
+    public static void PlayerHPBuff(int health, int randHPBuff) {
+        //create a random number for buff
+        randHPBuff = (int) ((Math.random() * 6) + 1) * 10;
+
+        //add random number to health value
+        health += randHPBuff;
+
+        //print message notifiying player incrase of Health stats
+        System.out.println("you've got a health potion! your health has increased " + randHPBuff);
+    }
+
+    /*
+     * Atchya Nandan, I am creating a set method for the name
+     */
+
+    //create a method that increases the players strength by a random value with potions
+    public static void PlayerATKBuff(int ATK, int randATKBuff) {
+        //create a random number for the strength
+        randATKBuff = (int) ((Math.random() * 6) + 1) * 2;
+
+        //add random number to the health
+        ATK += randATKBuff;
+
+        //print message notifiying player incrase of ATK stats
+        System.out.println("you've recieved a strength potion! your health has increased " + randATKBuff);
+
+    }
+
+    /*
+     * Atchya Nandan, I am creating a method that uses health and sets the base value to 100
+     */
+
+    //get method for players name
+    public String getstrName() {
+        return this.strName();
+    }
+
+    /*
+     * Atchya Nandan, I am creating a method that uses ATK and sets the base value to 35
+     */
+
+    //set method for players name
+    public void setstrName(String s) {
+        this.strName = s;
+    }
+
+    //create a method that gets the health of the player and reutns the health
+    public void health() {
+        //sets the base value of the health to 100
+        this.intHealth = 100;
+    }
+
+    /*
+     * Atchya Nandan, I am creating a get method for the health
+     */
+
+    //create method that sets ATK to 35
+    public void ATK() {
+        //sets the base value of ATK to 35
+        this.intATK = 35;
+    }
+
+    /*
+     * Atchya Nandan, I am creating a name method that asks the
+     * user for their name and then sets the name in the
+     * instance variable
+     */
+
+    /**
+     * Description: This is a get method that returns the attack value of the player class
+     *
+     * @author John Khalife
+     * @version 2021-06-16
+     *
+     */
+    public int getIntATK() { return this.intATK; }
+
+    //get method for players health
+    public int getintHealth() {
+        return this.intHealth();
+    }
+
+    //create method that asks the user for there name
+    public void name() {
+        //declare variable for the players name
+        String strPlayerName;
+
+        //declare a variable of type Player
+        Player name = new Player();
+
+        //ask the user for enter name
+        System.out.println("****************");
+        System.out.println("Enter your name");
+        System.out.println("****************");
+
+        //use the scanner to get name
+        strPlayerName = new Scanner(System.in).nextLine();
+
+        //set the users name with set method
+        name.setstrName(strPlayerName);
+
+    }
 }
 
 //Vincent *
